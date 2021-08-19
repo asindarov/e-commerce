32fa4231a55aa3be9c6f401eb3ffc61054408157
Asadbek07 <asindarov07@gmail.com>
2021-08-19 18:57:42 +0500

    first commit

---
+++ orders/tasks.py
@@ -0,0 +1,16 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+@task
+def order_created(order_id):
+	"""
+	Task to send an e-mail notification when an order is successfully created.
+	"""
+	order = Order.objects.get(id=order_id)
+	subject = f"Oder nr. {order.id}"
+	message = f"Dear {order.first_name}.\n\n"\
+			  f"You have successfully placed an order."\
+			  f"Your order ID is {order.id}."
+	mail_sent = send_mail(subject, message, "asindarov@gmail.com", [order.email])
+	return mail_sent		  	
